285558d1de77cacf2779c390cb8e8c975ea69a53
Daria Brinza <dahis.br@gmail.com>
2025-02-04 15:01:37 +0200

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        // Remove the default colors when finished
         foreground: "var(--foreground)",
+        // Main colors
+        background: "var(--background)",
+        primary: {
+          DEFAULT: "#6C79D1",
+          100: "#EDEEF3",
+          200: "#7A86D3",
+        },
+        black: {
+          DEFAULT: "#1E2329",
+        },
+        white: {
+          DEFAULT: "#F9F9FB",
+        },
       },
     },
   },
